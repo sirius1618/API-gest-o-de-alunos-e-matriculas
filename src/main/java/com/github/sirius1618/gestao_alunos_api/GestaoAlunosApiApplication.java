edcfde757bebe50cbd905bb693e68d0c024a54e9
sirius1618 <sagittarius_a1.618@proton.me>
2025-12-29 09:00:56 -0300

    init: Estrutura do projeto spring-boot

---
+++ src/main/java/com/github/sirius1618/gestao_alunos_api/GestaoAlunosApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.sirius1618.gestao_alunos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoAlunosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoAlunosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/sirius1618/gestao_alunos_api/GestaoAlunosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sirius1618.gestao_alunos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoAlunosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
