c0b99739e10581032810ea1910fd01ae7810d7c4
sirius1618 <sagittarius_a1.618@proton.me>
2025-12-30 13:36:59 -0300

    feat:Contruindo a logica do model aluno

--- src/main/java/com/github/sirius1618/gestao_alunos_api/model/Aluno.java
+++ src/main/java/com/github/sirius1618/gestao_alunos_api/model/Aluno.java
@@ -1,4 +1,45 @@
 package com.github.sirius1618.gestao_alunos_api.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_aluno")
 public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Column(length = 20)
+    private String telefone;
+
+    private LocalDate dataNascimento;
+
+    @Column(updatable = false)
+    private LocalDateTime dataInclusao;
+
+    private LocalDateTime dataAtualizacao;
+
+    @OneToMany(mappedBy = "aluno", cascade = CascadeType.ALL , orphanRemoval = true)
+    private List<Matricula> matriculas;
+
+    @PrePersist
+    protected void inicializarData() {
+        this.dataInclusao = LocalDateTime.now();
+        this.dataAtualizacao = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void atualizandoData() {
+        this.dataAtualizacao = LocalDateTime.now();
+    }
+
+
 }
