1fb093265656f6b693853b63f49bb186fe37cb17
sirius1618 <sagittarius_a1.618@proton.me>
2025-12-31 18:48:55 -0300

    style: Formatando para o estilo google

--- src/main/java/com/github/sirius1618/gestao_alunos_api/model/Matricula.java
+++ src/main/java/com/github/sirius1618/gestao_alunos_api/model/Matricula.java
@@ -1,26 +1,26 @@
 package com.github.sirius1618.gestao_alunos_api.model;
 
 import jakarta.persistence.*;
-
-import java.text.DateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "tb_matricula")
 public class Matricula {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String coidgoMatricula;
-    private String nomeCurso;
-    private LocalDate dataInicio;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @Column(updatable = false)
-    private LocalDateTime dataInclusao;
-    private LocalDateTime dataAtualizacao;
+  private String coidgoMatricula;
+  private String nomeCurso;
+  private LocalDate dataInicio;
 
-    @ManyToOne // muitas matriculas para um aluno
-    @JoinColumn(name = "aluno_id")
-    private Aluno aluno;
+  @Column(updatable = false)
+  private LocalDateTime dataInclusao;
+
+  private LocalDateTime dataAtualizacao;
+
+  @ManyToOne
+  @JoinColumn(name = "aluno_id")
+  private Aluno aluno;
 }
