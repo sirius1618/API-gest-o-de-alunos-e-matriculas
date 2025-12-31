74d8fcbbcbc4125f9f1240cb0a8f4bfefadbd2da
sirius1618 <sagittarius_a1.618@proton.me>
2025-12-31 18:47:10 -0300

    style: Formatando codigo deixando no formato google

--- src/main/java/com/github/sirius1618/gestao_alunos_api/model/Aluno.java
+++ src/main/java/com/github/sirius1618/gestao_alunos_api/model/Aluno.java
@@ -1,7 +1,6 @@
 package com.github.sirius1618.gestao_alunos_api.model;
 
 import jakarta.persistence.*;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -10,36 +9,34 @@ import java.util.List;
 @Table(name = "tb_aluno")
 public class Aluno {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @Column(nullable = false, length = 150)
-    private String nome;
+  @Column(nullable = false, length = 150)
+  private String nome;
 
-    @Column(length = 20)
-    private String telefone;
+  @Column(length = 20)
+  private String telefone;
 
-    private LocalDate dataNascimento;
+  private LocalDate dataNascimento;
 
-    @Column(updatable = false)
-    private LocalDateTime dataInclusao;
+  @Column(updatable = false)
+  private LocalDateTime dataInclusao;
 
-    private LocalDateTime dataAtualizacao;
+  private LocalDateTime dataAtualizacao;
 
-    @OneToMany(mappedBy = "aluno", cascade = CascadeType.ALL , orphanRemoval = true)
-    private List<Matricula> matriculas;
+  @OneToMany(mappedBy = "aluno", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<Matricula> matriculas;
 
-    @PrePersist
-    protected void inicializarData() {
-        this.dataInclusao = LocalDateTime.now();
-        this.dataAtualizacao = LocalDateTime.now();
-    }
+  @PrePersist
+  protected void inicializarData() {
+    this.dataInclusao = LocalDateTime.now();
+    this.dataAtualizacao = LocalDateTime.now();
+  }
 
-    @PreUpdate
-    protected void atualizandoData() {
-        this.dataAtualizacao = LocalDateTime.now();
-    }
-
-
+  @PreUpdate
+  protected void atualizandoData() {
+    this.dataAtualizacao = LocalDateTime.now();
+  }
 }
