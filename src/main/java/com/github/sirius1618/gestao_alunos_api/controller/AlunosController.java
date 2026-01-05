5e14db6ca048f9824804d31e08e07f3f0d07522b
sirius1618 <sagittarius_a1.618@proton.me>
2026-01-05 18:43:48 -0300

    refactor(controller): padroniza endpoint de alunos e aplica validações
    - Altera injeção de dependência para o modelo via construtor.
    - Ajusta a URL para focar no recurso (/alunos) seguindo o padrão REST.
    - Implementa DTOs distintos para entrada (Request) e saída (Response).
    - Garante o retorno HTTP 201 (Created) para sucesso.
    - Validação automática via @Valid retornando HTTP 400 em caso de erro.

--- src/main/java/com/github/sirius1618/gestao_alunos_api/controller/AlunosController.java
+++ src/main/java/com/github/sirius1618/gestao_alunos_api/controller/AlunosController.java
@@ -1,14 +1,29 @@
 package com.github.sirius1618.gestao_alunos_api.controller;
 
+import com.github.sirius1618.gestao_alunos_api.DTOs.CadastrarAlunoDTO;
 import com.github.sirius1618.gestao_alunos_api.model.Aluno;
+import com.github.sirius1618.gestao_alunos_api.service.AlunoService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/getao-alunos")
+@RequestMapping("/alunos")
 public class AlunosController {
-    @PostMapping("/cadastrar-alunos")
-    public void cadastrarAluno(@ResponseBody Aluno aluno) {
 
+    private final AlunoService alunoService;
 
+    public AlunoController(AlunoService alunoService) {
+        this.alunoService = alunoService;
     }
+
+  @PostMapping
+  public ResponseEntity<ResponsetCadastrarAlunoDTO> cadastrarAluno(@RequestBody @Valid  CadastrarAlunoDTO dados) {
+//        TODO: ver se precisa alterar e por que precisa alterar
+    var aluno = alunoService.salvar(dados);
+    return ResponseEntity.status(HttpStatus.CREATED).body(aluno); // 201
+  }
 }
---
+++ src/main/java/com/github/sirius1618/gestao_alunos_api/service/AlunoService.java
@@ -0,0 +1,4 @@
+package com.github.sirius1618.gestao_alunos_api.service;
+
+public class AlunoService {
+}
