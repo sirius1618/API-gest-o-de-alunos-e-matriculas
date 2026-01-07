d762d30357ff013e4d00a89ce051cdee9792fcb4
sirius1618 <sagittarius_a1.618@proton.me>
2026-01-07 13:58:06 -0300

    refactor(AlunosController): Arrumando nomes de importações e de variaveis, para corresponder as importações

--- src/main/java/com/github/sirius1618/gestao_alunos_api/controller/AlunosController.java
+++ src/main/java/com/github/sirius1618/gestao_alunos_api/controller/AlunosController.java
@@ -1,12 +1,10 @@
 package com.github.sirius1618.gestao_alunos_api.controller;
 
-import com.github.sirius1618.gestao_alunos_api.DTOs.CadastrarAlunoDTO;
-import com.github.sirius1618.gestao_alunos_api.model.Aluno;
+import com.github.sirius1618.gestao_alunos_api.DTOs.RequestCadastrarAlunoDTO;
+import com.github.sirius1618.gestao_alunos_api.DTOs.ResponsetCadastrarAlunoDTO;
 import com.github.sirius1618.gestao_alunos_api.service.AlunoService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,13 +14,12 @@ public class AlunosController {
 
     private final AlunoService alunoService;
 
-    public AlunoController(AlunoService alunoService) {
+    public AlunosController(AlunoService alunoService) {
         this.alunoService = alunoService;
     }
 
   @PostMapping
-  public ResponseEntity<ResponsetCadastrarAlunoDTO> cadastrarAluno(@RequestBody @Valid  CadastrarAlunoDTO dados) {
-//        TODO: ver se precisa alterar e por que precisa alterar
+  public ResponseEntity<ResponsetCadastrarAlunoDTO> cadastrarAluno(@RequestBody @Valid RequestCadastrarAlunoDTO dados) {
     var aluno = alunoService.salvar(dados);
     return ResponseEntity.status(HttpStatus.CREATED).body(aluno); // 201
   }
